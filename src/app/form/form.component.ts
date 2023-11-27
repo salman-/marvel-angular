413178c752eecee15b5ccb3b2f4be71d3b71f513
lashksal <salman.lashkarara@diconium.com>
2023-11-27 11:45:00 +0100

    simplify component by extracting helper methods from the component

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -9,54 +9,25 @@ import {HelpersService} from './helpers.service'
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  providers:[HelpersService]
+  providers: [HelpersService]
 })
 export class FormComponent {
   @Output() outputEvent: EventEmitter<Marvel[]> = new EventEmitter<Marvel[]>();
 
-  constructor(private http: HttpClient, private customFunctions: HelpersService) {
-
+  constructor(private http: HttpClient, private helpersService: HelpersService) {
   }
 
   onSubmit(form: NgForm) {
-    let ts = this.customFunctions.getTimeStamp();
-    const hash = this.customFunctions.generateHash();
-    const limit = 100;
-    const apiKey = this.customFunctions.getPublicKey();
-    const parameters = this.customFunctions.buildRequestParameters(ts,hash,limit,apiKey);
-
-    console.log("parameters: "+parameters);
-
-    this.http.get('https://gateway.marvel.com/v1/public/characters?ts=1700990816517&hash=9b43e45a42515bc356934b4706aad8e0&limit=100&apikey=1098114a1b4e71641fb1e0a0184afbcf')
+    const apiEndpoint = this.helpersService.buildApiEndpoint();
+    this.http.get(apiEndpoint)
       .pipe(map(responseData => {
 
-        let marvelWithThumbnail = this.filterMarvelsWithoutThumbnail(responseData);
-        return this.randomlySelectMarvels(marvelWithThumbnail);
+        let marvelWithThumbnail = this.helpersService.filterMarvelsWithoutThumbnail(responseData);
+        return this.helpersService.randomlySelectMarvels(marvelWithThumbnail);
       }))
       .subscribe(marvels => {
         this.outputEvent.emit(marvels);
       });
   }
 
-  private randomlySelectMarvels(marvelWithThumbnail: Marvel[]) {
-    let randomlySelectedMarvel: Marvel[] = [];
-    for (let i = 0; i < 6; i++) {
-      let index = Math.floor(Math.random() * marvelWithThumbnail.length);
-      randomlySelectedMarvel.push(marvelWithThumbnail[index]);
-    }
-    return randomlySelectedMarvel;
-  }
-
-  private filterMarvelsWithoutThumbnail(responseData: Object) {
-    let marvelWithThumbnail:Marvel[] = [];
-    for (let i = 0; i < 100; i++) {
-      let name = responseData['data']['results'][i]['name'];
-      let thumbnailOfMarvel = responseData['data']['results'][i]['thumbnail']['path'];
-      let validMarvel = new HelpersService().doesNotContainSubstring(thumbnailOfMarvel, "image_not_available");
-      if (validMarvel) {
-        marvelWithThumbnail.push(new Marvel(name, thumbnailOfMarvel + "/portrait_xlarge.jpg"));
-      }
-    }
-    return marvelWithThumbnail;
-  }
 }
--- src/app/form/helpers.service.ts
+++ src/app/form/helpers.service.ts
@@ -1,5 +1,6 @@
-import {environment} from './../environment/environment.prod';
+import {environment} from '../environment/environment.prod';
 import * as CryptoJS from 'crypto-js';
+import {Marvel} from "./marvel.model";
 
 export class HelpersService {
 
@@ -28,7 +29,49 @@ export class HelpersService {
     return environment.privateKey;
   }
 
+  getBaseUrl(){
+    return environment.baseUrl;
+  }
+
+  getProtocol(){
+    return environment.protocol;
+  }
+
   buildRequestParameters(ts: number, hash: any, limit: any, apikey: any): string {
     return `ts=${ts}&hash=${hash}&limit=${limit}&apikey=${apikey}`
+  }
+
+  buildApiEndpoint() {
+    let ts = this.getTimeStamp();
+    const hash = this.generateHash();
+    const limit = 100;
+    const apiKey = this.getPublicKey();
+    const parameters = this.buildRequestParameters(ts, hash, limit, apiKey);
+    const protocol = this.getProtocol();
+    const baseUrl = this.getBaseUrl();
+
+    return protocol + baseUrl + 'v1/public/characters?' + parameters;
+  }
+
+  randomlySelectMarvels(marvelWithThumbnail: Marvel[]) {
+    let randomlySelectedMarvel: Marvel[] = [];
+    for (let i = 0; i < 6; i++) {
+      let index = Math.floor(Math.random() * marvelWithThumbnail.length);
+      randomlySelectedMarvel.push(marvelWithThumbnail[index]);
+    }
+    return randomlySelectedMarvel;
+  }
+
+  filterMarvelsWithoutThumbnail(responseData: Object) {
+    let marvelWithThumbnail: Marvel[] = [];
+    for (let i = 0; i < 100; i++) {
+      let name = responseData['data']['results'][i]['name'];
+      let thumbnailOfMarvel = responseData['data']['results'][i]['thumbnail']['path'];
+      let validMarvel = this.doesNotContainSubstring(thumbnailOfMarvel, "image_not_available");
+      if (validMarvel) {
+        marvelWithThumbnail.push(new Marvel(name, thumbnailOfMarvel + "/portrait_xlarge.jpg"));
+      }
+    }
+    return marvelWithThumbnail;
   }
 }
